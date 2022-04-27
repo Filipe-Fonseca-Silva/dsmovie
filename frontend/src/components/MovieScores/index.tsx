020df54f21a482d4bf70072a7e7d4d9228f81e17
Filipe-Fonseca-Silva <fonseca82645@hotmail.com>
2022-04-27 14:08:09 -0300

    Security config

--- frontend/src/components/MovieScores/index.tsx
+++ frontend/src/components/MovieScores/index.tsx
@@ -5,7 +5,7 @@ function MovieScore() {
 
     const score = 3.5;
     const count = 13;
-debugger
+    
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
